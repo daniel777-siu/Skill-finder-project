d693bf4107d5a2ee21db51f9dbf081644f58a582
TEQUIE835 <david123orjuela@gmail.com>
2025-08-26 09:36:17 -0500

    upload a register function

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
-const { generateToken, createAuthCookie, comparePassword } = require("../utils/auth.utils");
+const { generateToken, createAuthCookie, comparePassword, hashPassword } = require("../utils/auth.utils");
 const client = require('../models/auth.model');
+const db = require ('../config/db.js');
 
-exports.loginUser = async (req, res) => {
+exports.loginUser = (req, res) => {
     const { email, password } = req.body;
    client.getOne(email, async (err, results) => {
     if (err) {
@@ -31,4 +32,43 @@ exports.loginUser = async (req, res) => {
       user: { id: user.id, email: user.email, role: user.role }
     });
   });
+};
+
+exports.registerUser = async (req, res) => {
+  const data = req.body;
+  const hashedPassword = await hashPassword(data.password)
+  db.query('SELECT id FROM places WHERE city = ?',
+    [data.place],
+    (err, results) => {
+      if (err) return (err);
+       if (results.length > 0) {
+        const place_id = results[0].id; 
+        db.query('SELECT id FROM clans WHERE clan_name = ?',
+          [data.clan],
+          (err, results) => {
+            if (err) return (err);
+            if (results.length > 0) {
+            const clan_id = results[0].id;
+              const modifiedData = {
+                password : hashedPassword,
+                place_id : place_id,
+                clan_id : clan_id
+              };
+                client.create(data, modifiedData, (err, result) =>{
+                if (err) return res.status(500).json({error: err.message});
+                res.json({message : "User succesfully created"});
+              });
+              
+            } else {
+              console.log("No se encontró el clan");
+            }
+          }
+        );
+    } else {
+      console.log("No se encontró el clan");
+    }
+    }
+  );
+  
+  
 };
